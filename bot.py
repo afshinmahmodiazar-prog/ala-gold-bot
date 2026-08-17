892f23d714d48dbe9e2f963416f0c26083ec6cf2
afshinmahmodiazar-prog <afshin.mahmodiazar@gmail.com>
2026-08-17 10:07:28 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import re
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHANNEL = "@alagoold"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/130 Safari/537.36"
 }
+
 
 def get_price(page_id):
     url = f"https://www.tgju.org/profile/{page_id}"
@@ -40,28 +47,64 @@ def get_price(page_id):
 
 
 items = {
-    "🟡 طلای ۱۸ عیار": "geram18",
-    "💵 دلار": "price_dollar_rl",
-    "💶 یورو": "price_eur",
-    "🇮🇶 دینار عراق": "price_iqd",
-    "🇹🇷 لیر ترکیه": "price_try"
+    "طلای ۱۸ عیار": "geram18",
+    "دلار": "price_dollar_rl",
+    "یورو": "price_eur",
+    "دینار عراق": "price_iqd",
+    "لیر ترکیه": "price_try"
 }
 
-print("شروع دریافت قیمت‌ها...")
-print("-" * 40)
+
+prices = {}
 
 for name, page_id in items.items():
+    rial_price = get_price(page_id)
+    prices[name] = rial_price // 10
 
-    try:
-        rial_price = get_price(page_id)
 
-        toman_price = rial_price // 10
+iran_time = datetime.now(
+    ZoneInfo("Asia/Tehran")
+).strftime("%H:%M")
 
-        print(f"{name}: {toman_price:,} تومان")
 
-    except Exception as e:
+message = f"""🟡 قیمت لحظه‌ای طلا و ارز
 
-        print(f"{name}: ERROR -> {e}")
+🟡 طلای ۱۸ عیار: {prices["طلای ۱۸ عیار"]:,} تومان
 
-print("-" * 40)
-print("تست دریافت قیمت‌ها تمام شد.")
+💵 دلار: {prices["دلار"]:,} تومان
+💶 یورو: {prices["یورو"]:,} تومان
+🇮🇶 دینار عراق: {prices["دینار عراق"]:,} تومان
+🇹🇷 لیر ترکیه: {prices["لیر ترکیه"]:,} تومان
+
+🕐 آخرین بروزرسانی: {iran_time}
+
+💎 زرگری ئالا
+
+خرید و فروش آبشده و طلای دست دوم بدون اجرت
+
+📞 تماس:
+09141661837
+09141661727
+09144407480
+
+📲 @alagoold
+"""
+
+
+telegram_url = (
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+)
+
+response = requests.post(
+    telegram_url,
+    data={
+        "chat_id": CHANNEL,
+        "text": message
+    },
+    timeout=30
+)
+
+print("Telegram status:", response.status_code)
+print("Telegram response:", response.text)
+
+response.raise_for_status()
